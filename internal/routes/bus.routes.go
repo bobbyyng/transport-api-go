3bb37aa43f2bcd6f42624aa06993ca3666043161
bobbyyng <httbrs116@gmail.com>
2025-01-09 00:46:22 +0800

    chore: route - bus API first version completed

--- internal/routes/bus.routes.go
+++ internal/routes/bus.routes.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
 	"transport-api/internal/models"
 
 	"github.com/gorilla/mux"
@@ -27,32 +29,87 @@ func (impl *BusImpl) GetNearestStops(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	latitude := vars["latitude"]
 	longitude := vars["longitude"]
+	queryParams := r.URL.Query()
+	searchNearestStopsLimit := queryParams.Get("searchNearestStopsLimit")
 
-	var routes []*models.Route
-	routes, _ = models.GetActiveRoutesByTransportType(r.Context(), impl.DB, 1)
+	limit := 30
+	if searchNearestStopsLimit != "" {
+		parsedLimit, err := strconv.Atoi(searchNearestStopsLimit)
+		if err != nil {
+			http.Error(w, "Invalid searchNearestStopsLimit", http.StatusBadRequest)
+			return
+		}
+		limit = parsedLimit
+	}
+
 	var nearestStops []models.NearestStop
-	nearestStops, _ = models.GetActiveNearestStops(r.Context(), impl.DB, latitude, longitude, 10)
+	nearestStops, _ = models.GetActiveNearestStops(r.Context(), impl.DB, 1, latitude, longitude, limit)
 
 	response := map[string]interface{}{
-		"data": routes,
+		"data": map[string]interface{}{},
 		"meta": map[string]interface{}{
-			"latitude":  latitude,
-			"longitude": longitude,
+			"request_info": map[string]interface{}{
+				"timestamp": time.Now().UTC().Format(time.RFC3339),
+				"latitude":  latitude,
+				"longitude": longitude,
+			},
 		},
 	}
 
-	for index, nearestStop := range nearestStops {
-		if index > 10 {
-			continue
-		}
+	nearestRoutes := []map[string]interface{}{}
+	for _, nearestStop := range nearestStops {
+		stop, _ := models.GetNearestStopByStopId(r.Context(), impl.DB, nearestStop.Stop)
 
 		var routeStops []*models.RouteStop
 		routeStops, _ = models.GetActiveRouteStopsByStopId(r.Context(), impl.DB, 1, nearestStop.Stop, "1")
 		for _, routeStop := range routeStops {
 			route, _ := models.GetFirstActiveRouteByTransportTypeAndRouteStop(r.Context(), impl.DB, 1, routeStop)
+			if route == nil {
+				continue
+			}
 
+			company, _ := models.GetCompanyById(r.Context(), impl.DB, *route.CompanyID)
+
+			route_info := map[string]interface{}{
+				"route": route.Route,
+				"bound": route.Bound,
+				"service": map[string]interface{}{
+					"type": route.ServiceType,
+					"name": route.ServiceNameEn,
+				},
+				"origin":      route.OriginEn,
+				"destination": route.DestinationEn,
+				"company": map[string]interface{}{
+					"slug":         company.SlugEn,
+					"display_slug": company.SlugEn,
+					"name":         company.NameEn,
+					"meta": map[string]interface{}{
+						"bg_color": company.BgColor,
+						"color":    company.TextColor,
+					},
+				},
+				"is_collaborated":  false,
+				"is_available_now": map[string]interface{}{},
+			}
+
+			nearestStop := map[string]interface{}{
+				"stop":               nearestStop.Stop,
+				"sequence":           routeStop.Sequence,
+				"name":               stop.NameEn,
+				"latitude":           stop.Latitude,
+				"longitude":          stop.Longitude,
+				"distance_from_user": nearestStop.Distance,
+			}
+
+			nearestRoute := map[string]interface{}{
+				"route_info":   route_info,
+				"nearest_stop": nearestStop,
+			}
+
+			nearestRoutes = append(nearestRoutes, nearestRoute)
 		}
 	}
+	response["data"] = nearestRoutes
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(response); err != nil {
