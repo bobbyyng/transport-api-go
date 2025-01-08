c2ce2be95ebcf483d9af2eeee7b114f97a168cff
bobbyyeung81 <bobby.yeung@vioo.com.hk>
2025-01-08 18:31:06 +0800

    chore: model - route add GetFirstActiveRouteByTransportTypeAndRouteStop function

--- internal/models/route.go
+++ internal/models/route.go
@@ -39,7 +39,8 @@ type Route struct {
 var routeColumns = []string{"id", "type", "route", "route_slug", "bound", "service_type", "service_name_tc", "service_name_sc", "service_name_en", "origin_en", "origin_tc", "origin_sc", "destination_en", "destination_tc", "destination_sc", "is_active", "created_at", "updated_at", "company_id", "region_slug", "route_id", "route_en", "route_tc", "route_sc"}
 
 func GetActiveRoutesByTransportType(ctx context.Context, dbpool *pgx.Conn, transportType int) ([]*Route, error) {
-	query := fmt.Sprintf("SELECT %s FROM tbl_route WHERE is_active = true AND type = %d Order By id ASC", strings.Join(routeColumns, ","), transportType)
+	query := fmt.Sprintf("SELECT %s FROM tbl_route WHERE is_active = true AND type = %d Order By id ASC",
+		strings.Join(routeColumns, ","), transportType)
 	rows, err := dbpool.Query(ctx, query)
 	if err != nil {
 		return nil, err
@@ -84,4 +85,53 @@ func GetActiveRoutesByTransportType(ctx context.Context, dbpool *pgx.Conn, trans
 	}
 
 	return routes, nil
+}
+
+func GetFirstActiveRouteByTransportTypeAndRouteStop(ctx context.Context, dbpool *pgx.Conn, transportType int, routeStop *RouteStop) (*Route, error) {
+	query := fmt.Sprintf("SELECT %s FROM tbl_route WHERE is_active = true AND type = %d AND company_id = %d AND route = '%s' AND bound = '%s' AND service_type = '%s' Order By id DESC",
+		strings.Join(routeColumns, ","), transportType, *routeStop.CompanyId, routeStop.Route, routeStop.Bound, routeStop.ServiceType)
+	rows, err := dbpool.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var routes []*Route
+	for rows.Next() {
+		var route Route
+
+		err := rows.Scan(
+			&route.ID,
+			&route.Type,
+			&route.Route,
+			&route.RouteSlug,
+			&route.Bound,
+			&route.ServiceType,
+			&route.ServiceNameTc,
+			&route.ServiceNameSc,
+			&route.ServiceNameEn,
+			&route.OriginEn,
+			&route.OriginTc,
+			&route.OriginSc,
+			&route.DestinationEn,
+			&route.DestinationTc,
+			&route.DestinationSc,
+			&route.IsActive,
+			&route.CreatedAt,
+			&route.UpdatedAt,
+			&route.CompanyID,
+			&route.RegionSlug,
+			&route.RouteID,
+			&route.RouteEn,
+			&route.RouteTc,
+			&route.RouteSc,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		routes = append(routes, &route)
+	}
+
+	return routes[0], nil
 }
